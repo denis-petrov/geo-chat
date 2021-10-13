44930b6315a4886d446a1d063a6d98a90237352d
Andrey Okunev <endryondrop@gmail.com>
2021-10-14 01:40:56 +0300

    Added search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,21 @@
+import {Button, Form} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLongArrowAltRight} from "@fortawesome/free-solid-svg-icons";
+import "../../assets/css/Search.css";
+
+export default function Search() {
+    return (
+        <div className={"px-3 py-4 bg-transparent"}>
+            <div className="d-flex block-round search">
+                <input type="text" placeholder="Search..."
+                              className={"border-0 py-1 px-3 w-100 rounded-left bg-transparent"}/>
+                <div className={"my-1 bg-secondary stick"} />
+                <div className="input-group-append">
+                    <Button className={"border-0 bg-transparent rounded-right shadow-none"}>
+                        <FontAwesomeIcon icon={faLongArrowAltRight} className={"text-dark"}/>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
