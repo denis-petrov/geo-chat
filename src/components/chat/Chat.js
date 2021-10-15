fa77a578c6879eb859ba1c7d7693fb170341d39c
Andrey Okunev <endryondrop@gmail.com>
2021-10-16 01:18:25 +0300

    Refactored chat page

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,16 +1,25 @@
-import "../../assets/css/Chat.css";
+import "../../assets/css/chat/Chat.css";
 import DialogItem from "./DialogItem";
 import Search from "../search/Search";
+import Navigation from "../navigation/Navigation";
 
-export default function Chat(props) {
+const Chat = (props) => {
     let items = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 12; i++) {
         items.push(<DialogItem/>);
     }
+
     return (
-        <>
-            <Search/>
-            {items}
-        </>
+        <div className={"chat"}>
+            <div className={"chat-wrapper mx-auto"}>
+                <Search/>
+                <div className={"dialog-items-wrapper"}>
+                    {items}
+                </div>
+                <Navigation/>
+            </div>
+        </div>
     );
 }
+
+export default Chat;
