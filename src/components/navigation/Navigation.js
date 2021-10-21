5ddb975c9fa367724b62b06b1e2521f7fdd15f47
denis-petrov <den11032001@mail.ru>
2021-10-21 23:18:23 +0300

    Create Map page

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,13 +1,26 @@
-import {Card} from "react-bootstrap";
-import "../../assets/css/navigation/Navigation.css";
+import {Card} from 'react-bootstrap'
+import '../../assets/css/navigation/Navigation.css'
+import {Link} from 'react-router-dom'
 
-const Navigation = (props) => (
-    <Card className={"navigation flex-row p-3"}>
-        <Card className={"navigation__item mx-auto block-round"}></Card>
-        <Card className={"navigation__item active mx-auto block-round"}></Card>
-        <Card className={"navigation__item mx-auto block-round"}></Card>
-        <Card className={"navigation__item mx-auto block-round"}></Card>
-    </Card>
-);
+const Navigation = () => {
+    console.log(window.location.pathname)
+    return (
+        <Card className={"navigation flex-row p-3"}>
+            <Card className={"navigation__item mx-auto block-round"}>
+                <Link to="/map">Map</Link>
+            </Card>
+            <Card className={"navigation__item active mx-auto block-round"}>
+                <Link to="/chat">Chat</Link>
+            </Card>
+            <Card className={"navigation__item mx-auto block-round"}>
+                <Link to="/profile">profile</Link>
+            </Card>
+            <Card className={"navigation__item mx-auto block-round"}>
+                <Link to="/settings">Settings</Link>
+            </Card>
+        </Card>
+    )
+}
 
-export default Navigation;
+
+export default Navigation
