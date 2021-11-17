99b1365daedd54eca184770608dfd7d8eaf5855c
denis-petrov <den11032001@mail.ru>
2021-11-17 23:53:19 +0300

    Update CSS

--- src/components/chat/ChatList.js
+++ src/components/chat/ChatList.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react';
-import '../../assets/css/chat/Chat.css';
-import DialogItem from './DialogItem';
-import Navigation from '../navigation/Navigation';
-import {connect} from "react-redux";
-import {getChats} from "../../store/actions/chat/getChats";
-import {addChat} from "../../store/actions/chat/addChat";
-import Add from "../navigation/control-button/Add";
-import Pages from "../navigation/Pages";
-import ChatSearch from "../navigation/search/ChatSearch";
-import {getMessages} from "../../store/actions/chat/getMessages";
+import React, {Component} from 'react'
+import '../../assets/css/chat/Chat.css'
+import DialogItem from './DialogItem'
+import Navigation from '../navigation/Navigation'
+import {connect} from "react-redux"
+import {getChats} from "../../store/actions/chat/getChats"
+import {addChat} from "../../store/actions/chat/addChat"
+import Add from "../navigation/control-button/Add"
+import Pages from "../navigation/Pages"
+import ChatSearch from "../navigation/search/ChatSearch"
+import {getMessages} from "../../store/actions/chat/getMessages"
 
 class ChatList extends Component {
 
     componentDidMount() {
-        console.log('test');
-        this.props.getChats('33f502fa-fe33-438b-8da3-5072d71444bc');
+        console.log('test')
+        this.props.getChats('33f502fa-fe33-438b-8da3-5072d71444bc')
     }
 
     render() {
-        let items = this.props.chats.chats;
-        console.log(this.props);
-        let dialogItems = [];
+        let items = this.props.chats.chats
+        console.log(this.props)
+        let dialogItems = []
         for (let key in items) {
-            let chat = items[key];
-            dialogItems.push(<DialogItem key={"dialog-item-" + chat.chatId} chatId={"" + chat.chatId} />);
+            let chat = items[key]
+            dialogItems.push(<DialogItem key={"dialog-item-" + chat.chatId} chatId={"" + chat.chatId}/>)
         }
 
-        const controlPanel = [<Add key={"Add"}/>];
+        const controlPanel = [<Add key={"Add"}/>]
 
         return (
             <div className={"chat"}>
@@ -35,10 +35,10 @@ class ChatList extends Component {
                     <div className={"dialog-items-wrapper"}>
                         {dialogItems}
                     </div>
-                    <Navigation currPage={Pages.CHAT} controlPanel={controlPanel} search={ChatSearch}/>
+                    <Navigation currPage={Pages.CHAT} controlPanel={controlPanel} search={<ChatSearch/>}/>
                 </div>
             </div>
-        );
+        )
     }
 }
 
