6b51f10a70fe37eb3bb90c29ad9ee151b22c131e
Andrey Okunev <endryondrop@gmail.com>
2021-11-17 23:15:14 +0300

    Small fix

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import markerReducer from './reducers/markerReducer'
 import centerPositionReducer from './reducers/centerPositionReducer'
-import userPositionReducer from './reducers/userPositionReducer'
 import chatReducer from './reducers/chatReducer';
 import userReducer from './reducers/userReducer';
 import messageReducer from './reducers/messageReducer';
@@ -9,7 +8,6 @@ import messageReducer from './reducers/messageReducer';
 export default combineReducers({
     markers: markerReducer,
     centerPosition: centerPositionReducer,
-    userPosition: userPositionReducer,
     chats: chatReducer,
     messages: messageReducer,
     user: userReducer
