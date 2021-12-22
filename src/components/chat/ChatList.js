5ac2b5f0baa3dd3ef529b2306d6f73338c4bbed7
Andrey Okunev <endryondrop@gmail.com>
2021-12-23 01:52:20 +0300

    Fixed chat list

--- src/components/chat/ChatList.js
+++ src/components/chat/ChatList.js
@@ -19,21 +19,23 @@ import {faPlus, faTimes} from "@fortawesome/free-solid-svg-icons";
 const ChatList = (props) => {
 
     const [isShowModal, setShowModal] = useState(false)
+    const [createChatModal, setCreateChatModal] = useState(false)
 
     useEffect(() => {
         let user = getCurrentUser()
         props.getChats(user.userId)
     }, [props.chats.inviteStatus])
 
-    const newChat = () => {
-        props.addChat('default chat')
+    const newChat = (chatName) => {
+        console.log(chatName)
+        props.addChat(chatName)
             .then((newChatId) => {
             if (newChatId) {
                 let user = getCurrentUser();
                 props.addMemberToChat(user.userId, newChatId);
-                window.location.assign(window.location.origin + '/chat/' + newChatId + '/settings');
+                window.location.assign(window.location.origin + '/chat/' + newChatId);
             }
-        });
+        })
     }
 
     const addMemberByInvite = () => {
@@ -58,7 +60,9 @@ const ChatList = (props) => {
         dialogItems.push(<h5 key={'empty'} className={"text-light text-center p-5"}>Chat list is empty</h5>)
     }
 
-    const controlPanel = [<Add key={"Add"} onClick={() => {newChat()}} />]
+    const controlPanel = [<Add key={"Add"} onClick={() => {
+        setCreateChatModal(true)
+    }} />]
 
     return (
         <div className={"chat"}>
@@ -95,6 +99,38 @@ const ChatList = (props) => {
                                         addMemberByInvite()
                                     }}>Find
                                     </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+
+            <Modal show={createChatModal}>
+                <div className={"chat"}>
+                    <div className={"chat-wrapper mx-auto"}>
+
+                        <div className={"px-4 py-2 bg-transparent dialog-header d-flex"}>
+                            <div className={"w-100"}>Create Chat</div>
+                            <FontAwesomeIcon icon={faTimes} className={"fa-lg my-auto ms-3"} onClick={() => {
+                                setCreateChatModal(false)
+                            }}/>
+                        </div>
+
+                        <div className={"dialog-items-wrapper p-3"}>
+                            <div className={"text-light py-2friend-list-content"}>
+                                <div className="input-group mb-3">
+                                    <input id="chat-name" type="text" className="form-control" placeholder="Chat Name"
+                                           aria-label="Chat Name" aria-describedby="basic-addon2"/>
+                                    <div className="input-group-append">
+                                        <button id={"create-chat"} className="btn btn-primary" type="button" onClick={() => {
+                                            let chatName = document.getElementById('chat-name').value.trim()
+                                            if (chatName.length > 0) {
+                                                newChat(chatName)
+                                            }
+                                        }}>Create Chat
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
